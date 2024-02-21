516471afdaa53602ef00e05f0c5879504567a130
MadeT <MadeT@ColinsZenbook.wusm-wifi.wucon.wustl.edu>
2024-02-21 15:56:07 -0600

    Studio4-Done-Added Tekla to group

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledRectangle(5,5, 5, 5);
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(0.5,0.5,.4, 0.25);
+		StdDraw.setPenColor(145,0,0);
+		StdDraw.filledEllipse(.5,0.5, 0.1, 0.1);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,39 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		int blueValue = in.nextInt();
+		Boolean isFilled = in.nextBoolean();
+		double pOne = in.nextDouble();
+		double pTwo = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		Color color = new Color(redValue, greenValue, blueValue);
+		StdDraw.setPenColor(color);
+		if (shapeType.equals("rectangle"))
+		{
+			if (isFilled == true)
+			{
+				StdDraw.filledRectangle(pOne, pTwo, halfWidth, halfHeight);
+			}
+			else 
+			{
+				StdDraw.rectangle(pOne, pTwo, halfWidth, halfHeight);
+			}
+		}
+		if (shapeType.equals("ellipse"))
+		{
+			if (isFilled == true)
+			{
+				StdDraw.filledEllipse(pOne, pTwo, halfWidth, halfHeight);
+			}
+			else 
+			{
+				StdDraw.ellipse(pOne, pTwo, halfWidth, halfHeight);
+			}
+		}
 		
 	}
 }
